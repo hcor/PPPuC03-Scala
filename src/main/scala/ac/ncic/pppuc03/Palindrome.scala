c9647b611ae5151930afe87e3b3134459fb9eb8e
anzhongqi <anzhongqi@gmail.com>
2015-05-28 17:37:56 +0800

    change `to` to `until`.

--- src/main/scala/ac/ncic/pppuc03/Palindrome.scala
+++ src/main/scala/ac/ncic/pppuc03/Palindrome.scala
@@ -10,7 +10,7 @@ object Palindrome {
       //p == p.reverse    // `reverse` is already implemented, so just call it; i know i'm lazy :)
 
       val len = p.length    // this is ugly, and is not functional at all.
-      for (i <- 0 to len/2-1)
+      for (i <- 0 until len/2)    // `to` includes the last value in the range, whereas `until` leaves it out.
         if (p.charAt(i) != p.charAt(len-i-1)) return false
       return true
     }
